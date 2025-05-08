5faf1ccf736302e305becdb321f251a61384b0eb
hikmetcanli <hikmet.canli@birinci.com>
2025-05-08 12:41:10 +0300

    DASHBOARD

--- Galeriden_com/Controllers/HomeController.cs
+++ Galeriden_com/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Galeriden_com.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Galeriden_com.Controllers
@@ -6,8 +7,22 @@ namespace Galeriden_com.Controllers
     [Authorize(Roles = "Admin")]
     public class HomeController : Controller
     {
+        Context c = new Context();
+
         public IActionResult Index()
         {
+            var FirmaSayisi = c.Musteri.Count();
+            ViewBag.FirmaSayisi = FirmaSayisi;
+            
+            var AracSayisi = c.Arac.Count();
+            ViewBag.AracSayisi = AracSayisi;
+
+            var ToplamSatinAlmaFiyati = c.SatinAlma.Sum(x=> x.AlimFiyati);
+            ViewBag.ToplamSatinAlmaFiyati = ToplamSatinAlmaFiyati;
+
+            var ToplamSatisFiyati = c.Satis.Sum(x => x.SatisFiyati);
+            ViewBag.ToplamSatisFiyati = ToplamSatisFiyati;
+
             return View();
         }
     }
